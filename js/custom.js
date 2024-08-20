5c27c2c8a02171bffafe8f3a345fb25bd97073f2
phamvanmung <113493773+phamvanmung@users.noreply.github.com>
2024-08-20 14:43:18 +0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-﻿// JavaScript Document
+// JavaScript Document
 
 $(window).load(function () {
     "use strict";
@@ -107,32 +107,41 @@ $(document).ready(function () {
     // Khởi tạo animatedModal cho mỗi liên kết
     $("#demo01").animatedModal({
         modalTarget: 'animatedModal',
+        animatedIn: 'zoomIn',
+        animatedOut: 'zoomOut',
+        duration : 1,
     });
 
     $("#demo02").animatedModal({
-        modalTarget: 'animatedModal1',
+        modalTarget: 'animatedModal-1',
+        animatedIn: 'zoomIn',
+        animatedOut: 'zoomOut',
+        duration: 1,
     });
-
-    //$("#btn-close-modal1").on("click", function () {
-    //    $("#animatedModal").animatedModal('close');
-    //});
-
-    //$("#btn-close-modal2").on("click", function () {
-    //    $("#animatedModal1").animatedModal('close');
-    //});
-
+    $("#demo03").animatedModal({
+        modalTarget: 'animatedModal-2',
+        animatedIn: 'zoomIn',
+        animatedOut: 'zoomOut',
+        duration: 1,
+    });
+    $("#demo04").animatedModal({
+        modalTarget: 'animatedModal-3',
+        animatedIn: 'zoomIn',
+        animatedOut: 'zoomOut',
+        duration: 1,
+    });
 
     //Phát hiện vuốt quay về (swipe right) để đóng modal
     $("#animatedModal").on("swiperight", function () {
         $("#animatedModal").animatedModal('close');
     });
-    $("#animatedModal1").on("swiperight", function () {
+    $("#animatedModal-1").on("swiperight", function () {
         $("#animatedModal1").animatedModal('close');
     });
     // Contact Form 	
 
      //validate contact form
-    $(function () { 
+    $(function () {
         $('#contact-form').validate({
             rules: {
                 name: {
@@ -148,43 +157,41 @@ $(document).ready(function () {
                 message: {
                     required: true
                 }
-
             },
             messages: {
                 name: {
-                    required: "This field is required",
-                    minlength: "your name must consist of at least 2 characters"
+                    required: "Trường này là bắt buộc",
+                    minlength: "Tên của bạn phải có ít nhất 2 ký tự"
                 },
                 email: {
-                    required: "This field is required"
+                    required: "Trường này là bắt buộc"
                 },
                 message: {
-                    required: "This field is required"
+                    required: "Trường này là bắt buộc"
                 }
             },
             submitHandler: function (form) {
-                $(form).ajaxSubmit({
-                    type: "POST",
+                $.ajax({
+                    url: $(form).attr('action'),
+                    method: 'POST',
                     data: $(form).serialize(),
-                    url: "process.php",
+                    dataType: 'json',
                     success: function () {
-                        $('#contact :input').attr('disabled', 'disabled');
-                        $('#contact').fadeTo("slow", 1, function () {
-                            $(this).find(':input').attr('disabled', 'disabled');
-                            $(this).find('label').css('cursor', 'default');
+                        $('#contact-form').fadeTo("slow", 1, function () {
                             $('#success').fadeIn();
+                            $('#contact-form').trigger("reset"); // Reset form sau khi gửi thành công
                         });
                     },
                     error: function () {
-                        $('#contact').fadeTo("slow", 1, function () {
+                        $('#contact-form').fadeTo("slow", 1, function () {
                             $('#error').fadeIn();
                         });
                     }
                 });
             }
         });
-
     });
+
 });
 window.addEventListener('load', function() {
     var loader = document.querySelector('.ui-loader.ui-corner-all.ui-body-a.ui-loader-default');
